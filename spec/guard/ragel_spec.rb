48303e5a7f0cae709d1f5cebc061d3c81b32c580
John Barker <jebarker@gmail.com>
2011-06-29 22:37:58 +0800

    Some extra specs to test handling errors and processing csharp files

--- spec/guard/ragel_spec.rb
+++ spec/guard/ragel_spec.rb
@@ -5,9 +5,18 @@ describe Guard::Ragel do
 
   before { subject.options[:output] = 'spec/assets' }
   
-  describe "initialize" do
-    it "should set default output format" do
+  describe "insitialize" do
+    it 'sets default output format' do
       subject.options[:output_format].should == :ruby
+    end
+
+    it 'sets default notification to on' do
+      subject.options[:notification].should be_true
+    end
+
+    it 'sets default ragel options to blank' do
+      subject.options[:options].should == ''
+
     end
   end
   
@@ -19,11 +28,10 @@ describe Guard::Ragel do
     end
   end
   
-  describe "building rl to rb" do
-
+  describe "building from rl" do
     it "should convert rl to rb" do
-      file = "spec/assets/hello_world.rl"
-      outfile = "spec/assets/hello_world.rb"
+      file = "spec/assets/hello_world_ruby.rl"
+      outfile = "spec/assets/hello_world_ruby.rb"
 
       begin
         subject.build_ragel(file).should == outfile
@@ -37,6 +45,31 @@ describe Guard::Ragel do
       other_guard.should_receive(:watchers).and_return([])
       Guard.stub(:guards).and_return([subject, other_guard])
       subject.notify([])
+    end
+  end
+
+  context 'csharp format' do
+    before { subject.options[:output_format] = :csharp }
+
+    it "should convert rl to cs" do
+      file = "spec/assets/hello_world_csharp.rl"
+      outfile = "spec/assets/hello_world_csharp.cs"
+
+      begin
+        subject.build_ragel(file).should == outfile
+      ensure
+        File.unlink outfile
+      end
+    end
+  end
+
+  context 'unsupported format' do
+    before { subject.options[:output_format] = :unsupported_format }
+
+    it 'raises an argument error' do
+      expect do
+        subject.build_ragel('spec/assets/hello_world_ruby.rl')
+      end.to raise_error(ArgumentError)
     end
   end
   
